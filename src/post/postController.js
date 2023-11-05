8d7994f1fa286c4b858763276e509a1fb9516e83
penguin-cho <penguin-cho@cloud-latitude.com>
2023-11-05 15:11:05 +0800

    controller add pos

--- src/post/postController.js
+++ src/post/postController.js
@@ -5,5 +5,11 @@ const postController = (app = require('express')(), service) => {
         const posts = await service.execute()
         res.send(posts)
     })
+
+    app.post('/post', async (req, res) => {
+        const {
+            title,
+        } = req.body
+    })
 }
 module.exports = postController;
