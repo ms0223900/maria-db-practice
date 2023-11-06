b0205b0854824d33495f016481824e09f157ed34
penguin-cho <penguin-cho@cloud-latitude.com>
2023-11-06 15:13:32 +0800

    add delete controller

---
+++ src/post/deletePostController.js
@@ -0,0 +1,14 @@
+
+
+const deletePostController = (app = require('express')(), service) => {
+    app.delete('/post/:id', async (req, res) => {
+        try {
+           const id = parseInt(req.params.id)
+           const deleted = await service.execute(id)
+           res.status(200).json(deleted)
+        } catch (e) {
+            res.status(400).json(e.message || e)
+        }
+    })
+}
+module.exports = {updatePostController: deletePostController};
