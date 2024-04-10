a80552bf4a7f31c5e7882cd5f40e7dda6b0437ee
penguin-cho <penguin-cho@cloud-latitude.com>
2024-04-10 21:32:15 +0800

    test: add query tag test

--- src/post/postController.test.js
+++ src/post/postController.test.js
@@ -3,8 +3,7 @@ const { Post } = require("./Post");
 
 jest.useFakeTimers();
 describe('post controller', function () {
-    function givenApp() {
-        let req = {};
+    function givenApp(req = {}) {
         const spySend = jest.fn();
         const res = {
             send: spySend,
@@ -12,8 +11,6 @@ describe('post controller', function () {
         const app = jest.fn();
         app.mockReturnValue({
             async get(_, cb) {
-                console.log("_: ", _);
-                console.log("cb: ", cb);
                 await cb(req, res)
             }
         })
@@ -51,13 +48,29 @@ describe('post controller', function () {
         return getPostsByTagIdService;
     }
 
-    it('all ok', async () => {
+    it('all ok when no req', async () => {
         const { spySend, app } = givenApp();
         const getPostsService = givenPostService();
         const postsByTagIdService = givenPostsByTagIdService();
 
-        await getPostsController(app(), getPostsService())
+        await getPostsController(app(), getPostsService(), postsByTagIdService())
         await expect(spySend).toHaveBeenCalled()
-
     });
+
+    it('all ok when req query have tag', async () => {
+        const { spySend, app } = givenApp({
+            query: { tag: 1 }
+        });
+        const getPostsService = givenPostService([]);
+        const response = [
+            Post({ id: 1 }),
+            Post({ id: 3 }),
+        ];
+        const postsByTagIdService = givenPostsByTagIdService(response);
+
+        await getPostsController(app(), getPostsService(), postsByTagIdService())
+        await expect(spySend).toBeCalledWith(response)
+    });
+
+
 });
